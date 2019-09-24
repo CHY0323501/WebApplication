5d3dba0cf8990e57dad3e15e68680d3a67b5c6f3
CHY0323501 <a0958288010@gmail.com>
2019-09-24 14:29:38 +0800

    go

---
+++ 03View/Models/Member.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _03View.Models
+{
+    public class Member
+    {
+        public string userid { get; set; }
+        public string name { get; set; }
+        public string pwd { get; set; }
+        public string email { get; set; }
+        public DateTime birthday { get; set; }
+    }
+}
